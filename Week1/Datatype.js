bb8c6fddc0cccd70e6b8f433f1d56f92368ec7e2
Logeswaran-25 <adrsme.logeswaran@gmail.com>
2025-12-07 01:22:04 +0530

    Add files via upload

---
+++ Week1/Datatype.js
@@ -0,0 +1,15 @@
+var browserversion=12.2
+var browsername="Chrome"
+var isbrowseropen=true
+var history=null
+
+console.log("browserversion:" +browserversion+"\n"+
+    "browsername:" +browsername+"\n"+
+    "isbrwseropen:"+isbrowseropen+"\n"+
+    "history:" +history
+)
+console.log(typeof browserversion )
+console.log(typeof browsername )
+console.log(typeof isbrowseropen)
+console.log(typeof history)
+
---
+++ Week1/Variabledeclare.js
@@ -0,0 +1,13 @@
+var num=45
+num=46
+console.log(typeof num +" "+ "re-assignment:"+num)
+var num=47
+console.log(typeof num +" "+"re-declaration:"+num)
+
+let empname="testing"
+empname="new test"
+console.log(typeof empname +" "+"re-assignmentet:"+empname)
+
+
+const final=34
+console.log(typeof final+ " "+ final)
